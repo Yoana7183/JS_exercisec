d346489a51196c2ac4ccb78fce5bc39e35ae8193
unknown <yoana.nakova@yahoo.com>
2022-08-05 00:01:19 +0300

    works

--- TodoApp/main.js
+++ TodoApp/main.js
@@ -1,82 +1,58 @@
-
 const todoArr = [];
 class Todo {
 
     constructor(todo) {
         this.todo = todo
         this.isDone = false;
-        this.id = (this.constructor.counter || 0) + 1;
-
+        this.constructor.counter = (this.constructor.counter || 0) + 1;
+        this.id = this.constructor.counter;
     }
-
-
 }
 
 const addTodo = () => {
-
     var inputValue = document.getElementById('inputValue');
     var inputValue = inputValue.value;
-    var todo = new Todo(inputValue);
+
     if (inputValue === "") {
         console.log('ad');
     } else {
-        todoArr.push(todo)
-        document.getElementById("displayTodos").innerHTML += `<span id="comp"><li  idx="${todoArr.length - 1}"id="todos">${todo.todo}</span> 
-        <button type="button"id="deleteTodo" onclick="deleteTodo()">Delete</button><button type="button"id="completeTodo" onclick="completeTodo()">Done</button></li>`
+        var todo = new Todo(inputValue);
+        todoArr.push(todo);
+        displayTodos();
     }
-
 }
 
 const deleteTodo = () => {
+
     const btn = event.target;
     const index = parseInt(btn.parentElement.getAttribute("idx"));
-    todoArr.splice(index, 1);
-    document.getElementById("displayTodos").innerHTML = todoArr.map((todo, i) => {
-        return `<span><li idx="${i}">${todo.todo} </span id="comp"><button id="deleteTodo"type="button" onclick="deleteTodo()">Delete</button><button id="completeTodo"type="button" onclick="completeTodo()">Done</button></li>`
-    });
+    todoArr.splice(index - 1, 1);
 
-
+    displayTodos();
 }
 
 
 const completeTodo = () => {
 
-    document.getElementById("displayTodos").innerHTML = todoArr.map((todo, i) => {
-        return `<span id="comp" ><li idx="${i}">${todo.todo}</span> <button id="deleteTodo"type="button" onclick="deleteTodo()">Delete</button><button id="completeTodo"type="button" onclick="completeTodo()">Done</button></li>`
-    });
+    const btn = event.target;
+    const index = parseInt(btn.parentElement.getAttribute("idx"));
+    var selectedTodo = todoArr[index - 1];
+    selectedTodo.isDone = !selectedTodo.isDone;
 
-    todoArr.isDone = !todoArr.isDone
-
-    if (todoArr.isDone === false) {
-          document.getElementById("comp").style.textDecoration = 'line-through'
-          todoArr.isDone!=todoArr.isDone
-         } if (todoArr.isDone === true) {
-             document.getElementById("comp").style.textDecoration = 'none'
-        }
-    console.log(todoArr.isDone);
-
+    displayTodos();
 }
 
 document.getElementById('addTodos').addEventListener("click", addTodo);
 
-// todoArr.isDone = !todoArr.isDone
-// if (element === false) {
-//     document.getElementById("displayTodos").style.textDecoration = 'line-through'
-//     element!=element
-// } if (element === true) {
-//     document.getElementById("displayTodos").style.textDecoration = 'none'
-// }
-// console.log(todoArr.isDone);
 
-
-
-
-
-
-
-
-
-
-document.getElementById('addTodos').addEventListener("click", addTodo);
-
-
+function displayTodos() {
+    document.getElementById("displayTodos").innerHTML = todoArr.map((todo, i) => {
+        if (todo.isDone) {
+            return ` <li style="text-decoration: line-through" id="comp" idx="${todo.id}">${todo.todo}</div> <button id="deleteTodo"type="button" onclick="deleteTodo()">Delete</button>
+            <button id="completeTodo"type="button" onclick="completeTodo()">Undone</button></li>`;
+        } else {
+            return `<li id="comp" idx="${todo.id}">${todo.todo}</div> <button id="deleteTodo"type="button" onclick="deleteTodo()">Delete</button>
+            <button id="completeTodo"type="button" onclick="completeTodo()">Done</button></li>`;
+        }
+    });
+}
