7ac3bc5d55d006f9bc83319403dabee2cd5532b5
y nakova <yoana.nakova@yahoo.com>
2022-10-26 14:04:35 +0300

    added 2func in warehouse class/added shop and client classes

--- WareHouse/index.js
+++ WareHouse/index.js
@@ -24,28 +24,21 @@ class ProductCategory {
 
     addProduct(product, quantity) {
         this.products.set(product, quantity)
-
-
     }
-
 }
 
 let prodCategory1 = new ProductCategory('FRUITS')
 prodCategory1.addProduct(product1, 15)
 prodCategory1.addProduct(product2, 15)
 prodCategory1.addProduct(product3, 15)
-    ;
-
 let prodCategory2 = new ProductCategory('VEGETABLES')
 prodCategory2.addProduct(product4, 1)
 prodCategory2.addProduct(product5, 15)
 prodCategory2.addProduct(product6, 15)
-
 let prodCategory3 = new ProductCategory('MEATS')
 prodCategory3.addProduct(product7, 5)
 prodCategory3.addProduct(product8, 15)
 prodCategory3.addProduct(product9, 15)
-
 
 class WareHouse {
     constructor(name) {
@@ -55,34 +48,47 @@ class WareHouse {
 
     addProductType(productType) {
         this.productType.push(productType)
-
     }
+
     getStokes() {
         console.log(this.productType);
     }
 
     StockDelivery() {
         let prod;
-        let key;
-        let quantity;
+
         for (let i = 0; i < this.productType.length; i++) {
-            
             prod = this.productType[i].products
-          
-            key = this.productType[i].products.keys()
-           quantity = prod.get(...key)
-           
-           console.log(quantity);
+
+            prod.forEach((quantity, p) => {
+                if (quantity <= 5) {
+                    prod.set(p, 25)
+                    this.getStokes()
+                }
+            });
         }
-        
-
-
-
-
+        return prod
     }
 
+    takingGoods(prodName) {
+        let prod;
 
+        for (let i = 0; i < this.productType.length; i++) {
+            prod = this.productType[i].products
 
+            prod.forEach((quantity, productName) => {
+                if (productName.product === prodName)
+                    if (quantity >= 8) {
+                        let upatetQuantity = quantity - 5
+                        prod.set(productName, upatetQuantity)
+                    } else {
+                        this.StockDelivery()
+                        this.getStokes()
+                    }
+            });
+
+        }
+    }
 }
 
 
@@ -92,4 +98,73 @@ wareHouse.addProductType(prodCategory2)
 wareHouse.addProductType(prodCategory3)
 
 
-wareHouse.StockDelivery()
+let supplierOfGoodsInTheWarehouse = {
+    name: 'Georgi',
+    age: '20',
+    checkForGoodsShortage: function (wareHouse) {
+        wareHouse.StockDelivery()
+        wareHouse.getStokes()
+    }
+}
+
+class Shop{
+    constructor(name){
+        this.name = name
+        this.stockInShop = []
+    }
+    addStokes(stokeGroup){
+        this.stockInShop.push(stokeGroup)
+        console.log(this.stockInShop);
+    }
+    getStokes(){
+       console.log(this.stockInShop);
+    }
+    takeGoodsFromWarehouse(wareHouse){
+        let prod;
+        let goodsTakenFromTheWarehouse ;
+
+        for (let i = 0; i < this.stockInShop.length; i++) {
+            prod = this.stockInShop[i].products
+
+            prod.forEach((quantity, p) => {
+                if (quantity <= 5) {
+                console.log(`lfkwlfj`);
+                    this.getStokes()
+                }
+            });
+        }
+        return prod
+    }
+
+
+    }
+
+
+
+
+class Client{
+    constructor(shop){
+        this.shop = shop
+    }
+}
+
+
+let shop1 = new Shop ('Shop One')
+shop1.addStokes(prodCategory1)
+shop1.addStokes(prodCategory2)
+shop1.addStokes(prodCategory3)
+shop1.takeGoodsFromWarehouse(wareHouse)
+// let shop2 = new Shop ('Shop Two')
+// let shop3 = new Shop ('Shop Three')
+// wareHouse.StockDelivery()
+// wareHouse.takingGoods('Pork')
+// wareHouse.takingGoods('Pork')
+// wareHouse.takingGoods('Pork')
+// wareHouse.takingGoods('Pork')
+// wareHouse.takingGoods('Pork')
+
+// supplierOfGoodsInTheWarehouse.checkForGoodsShortage(wareHouse)
+
+
+
+
