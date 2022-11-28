e645a4aca347cc4b97adbc638718b1fdb12b6516
Yoana <yoana.nakova@yahoo.com>
2022-11-28 15:16:10 +0200

    borrow/lend/donate/return/ all func works/all checks work

--- Library/index.js
+++ Library/index.js
@@ -14,6 +14,7 @@ class User {
         this.listOfBooks = []
         this.borrowedBooks = []
     }
+
     
     //add books in this.listOfBooks[] / personal list/ doesnt depend on borrowedBooks[]//
     addBooksInmyListOfBooks(book) {
